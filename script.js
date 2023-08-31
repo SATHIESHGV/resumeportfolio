22c89eb128e3361edd0e5efcfbd9f1e9d1562abf
SATHIESH GV <109450513+SATHIESHGV@users.noreply.github.com>
2023-08-31 17:35:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var tablinks =document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents){
+                tabcontent.classList.remove("active-tab");
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+        }
+ const scriptURL = '<https://script.google.com/macros/s/AKfycby0P28zlTLO8rNTHweoJnwazqH-cMiBDpq7HU8QfSayqsbcneMG_uxiMyKQ72yA52e6/exec>'
+                const form = document.forms['submit-to-google-sheet']
+
+                form.addEventListener('submit', e => {
+                    e.preventDefault()
+                    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                    .then(response => console.log('Success!', response))
+                    .catch(error => console.error('Error!', error.message))
+  })
+
+
